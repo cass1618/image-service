75f24e9c54e67a01ab33583519f680393775ff68
Cassandra Copp <cass16180@gmail.com>
2023-05-18 19:29:45 -0700

    add some basic error handling

--- api_requests.py
+++ api_requests.py
@@ -12,15 +12,22 @@ def getImage(image_id):
     # send get request
     response = requests.get(url)
 
-    # response returns a base64 encoded string
-    base64_string = response.json()['data']
+    if response.json()['statusCode'] == 200:
+        # response returns a base64 encoded string
+        base64_string = response.json()['data']
 
-    # convert string to a BytesIO object for use with PIL
-    image_file = BytesIO(base64.b64decode(base64_string))
+        # convert string to a BytesIO object for use with PIL
+        image_file = BytesIO(base64.b64decode(base64_string))
 
-    # convert BytesIO object to Image object
-    image = Image.open(image_file)
-    return image
+        # convert BytesIO object to Image object
+        image = Image.open(image_file)
+        return image
+
+    elif response.json()['statusCode'] == 413:
+        print("image not found")
+
+    else:
+        print("unknown error")
 
 
 def uploadImage(file_path, image_id):
@@ -38,7 +45,14 @@ def uploadImage(file_path, image_id):
     }
 
     # send post request
-    requests.post(url, json=body)
+    try:
+        response = requests.post(url, json=body)
+        response.raise_for_status()
+        print("successfully uploaded image")
+    except requests.exceptions.HTTPError as error:
+        print("file too large - max size 4MB") \
+            if response.status_code == 413 \
+            else print("unspecified error: ", error)
 
 
 def listImages():
